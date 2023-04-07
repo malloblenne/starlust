62f92317493305a5ead5a2f14079451fdaf3f60c
Mauro <malloblenne@gmail.com>
2023-04-07 10:51:21 +0200

    Run Rapier3d engine. - One plane for ground - One ball with gravity, bouncing - Camera can rotate based on keyboard - Other ball can float, gravity 0, like "Explorers" 1985. Keyboard changes linear speed.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy_rapier3d::prelude::*;
 
 //use std::f32::consts::PI;
 
@@ -10,7 +11,11 @@ struct Player;
 struct Movable;
 
 #[derive(Component)]
-struct Velocity{x: f32}
+struct MovableCamera;
+
+
+#[derive(Component)]
+struct VelocityCustom{x: f32}
 
 
 fn populate_world(mut commands: Commands,
@@ -34,21 +39,21 @@ fn populate_world(mut commands: Commands,
 
 
     // cube
-    commands.spawn((
-        PbrBundle {
-            //mesh: meshes.add(Mesh::from(shape::RegularPolygon{sides: 3, radius: 1.0})),//shape::Cube { size: 1.0 })),
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(StandardMaterial {
-                base_color: Color::LIME_GREEN,
-                ..default()
-            }),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..default()
-        },
-        Movable,
-        Player,
-        Velocity{x: 0.0},
-    ));
+    // commands.spawn((
+    //     PbrBundle {
+    //         //mesh: meshes.add(Mesh::from(shape::RegularPolygon{sides: 3, radius: 1.0})),//shape::Cube { size: 1.0 })),
+    //         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+    //         material: materials.add(StandardMaterial {
+    //             base_color: Color::LIME_GREEN,
+    //             ..default()
+    //         }),
+    //         transform: Transform::from_xyz(0.0, 0.5, 0.0),
+    //         ..default()
+    //     },
+    //     Movable,
+    //     Player,
+    //     VelocityCustom{x: 0.0},
+    // ));
 
     // ambient light
     commands.insert_resource(AmbientLight {
@@ -56,22 +61,67 @@ fn populate_world(mut commands: Commands,
         brightness: 0.92,
     });
 
-    // // camera
+    // camera
     // commands.spawn(Camera3dBundle {
     //     transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
     //     ..default()
     // });
     // camera
-    commands.spawn(Camera3dBundle {
-        projection: OrthographicProjection {
-            scale: 3.0,
-            scaling_mode: ScalingMode::FixedVertical(2.0),
-            ..default()
-        }
-        .into(),
-        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    // commands.spawn(Camera3dBundle {
+    //     projection: OrthographicProjection {
+    //         scale: 3.0,
+    //         scaling_mode: ScalingMode::FixedVertical(2.0),
+    //         ..default()
+    //     }
+    //     .into(),
+    //     transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    //     ..default()
+    // });
+
+    commands.spawn((Camera3dBundle {
+        transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        
+        ..Default::default()
+    }, MovableCamera));
+
+    /* Create the ground. */
+    commands
+        .spawn((Collider::cuboid(100.0, 0.1, 100.0),
+        TransformBundle::from(Transform::from_xyz(0.0,  -2.0, 0.0))
+        ));
+
+    // commands
+    //     .spawn((Collider::cuboid(10.0, 0.1, 10.0),
+    //     TransformBundle::from(Transform::from_xyz(0.0,  -1.0, 0.0))
+    //     ));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert((Collider::ball(0.5),
+        TransformBundle::from(Transform::from_xyz(1.0, 2.0, 0.0)),
+        Player))
+        .insert(Restitution::coefficient(0.7))
+        .insert(Velocity {
+            linvel: Vec3::new(0.0, 0.0, 0.0),
+            angvel: Vec3::new(0.2, 0.0, 0.0),
+        })
+        .insert(GravityScale(0.0))
+        .insert(Sleeping::disabled());
+
+        commands
+        .spawn(RigidBody::Dynamic)
+        .insert((Collider::ball(0.5),
+        TransformBundle::from(Transform::from_xyz(-5.0, 3.0, 0.0)),
+        Player))
+        .insert(Restitution::coefficient(1.0))
+        // .insert(Velocity {
+        //     linvel: Vec3::new(0.0, 0.0, 0.0),
+        //     angvel: Vec3::new(0.2, 0.0, 0.0),
+        // })
+        .insert(GravityScale(0.5))
+        .insert(Sleeping::disabled());
+
 
 }
 
@@ -80,11 +130,40 @@ pub struct StarLustPlugin;
 impl Plugin for StarLustPlugin {
     fn build(&self, app: &mut App) {
         //app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-        app.insert_resource(ClearColor(Color::BLACK))
-        .add_startup_system(populate_world)
-        .add_system(move_player);
+        //app.insert_resource(ClearColor(Color::BLACK))
+        app.add_startup_system(populate_world)
+        .add_system(move_player)
+        .add_system(move_camera)
+        .add_system(print_ball_altitude);
             //.add_system(hello_world)
             //.add_system(greet_people);
+    }
+}
+
+
+fn move_camera(
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut Transform, (With<MovableCamera>)>,
+) {
+    for mut transform  in &mut query {
+        let mut direction = Vec3::ZERO;
+        if input.pressed(KeyCode::W) {
+            direction.y += 1.0;
+        }
+        if input.pressed(KeyCode::S) {
+            direction.y -= 1.0;
+        }
+        if input.pressed(KeyCode::A) {
+            direction.x -= 1.0;
+        }
+        if input.pressed(KeyCode::D) {
+            direction.x += 1.0;
+        }
+
+        transform.rotate_axis(direction,
+                              time.delta_seconds() * 45f32.to_radians());
+
     }
 }
 
@@ -92,7 +171,7 @@ impl Plugin for StarLustPlugin {
 fn move_player(
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
-    mut query: Query<(&mut Transform, &mut Velocity), (With<Player>, With<Movable>)>,
+    mut query: Query<(&mut Transform, &mut Velocity), With<Player>>,
 ) {
     for (mut transform, mut velocity)  in &mut query {
         let mut direction = Vec3::ZERO;
@@ -110,24 +189,34 @@ fn move_player(
         }
 
         //transform.translation += time.delta_seconds() * 2.0 * direction;
-        transform.rotate_axis(direction,
-                              time.delta_seconds() * 45f32.to_radians());
+        // transform.rotate_axis(direction,
+        //                       time.delta_seconds() * 45f32.to_radians());
 
-        velocity.x += time.delta_seconds() * ((input.pressed(KeyCode::Space) as i32) as f32 - 0.1f32);
-        if velocity.x <= 0f32
-        {
-            velocity.x = 0f32;
-        }
+        // velocity.x += time.delta_seconds() * ((input.pressed(KeyCode::Space) as i32) as f32 - 0.1f32);
+        // if velocity.x <= 0f32
+        // {
+        //     velocity.x = 0f32;
+        // }
         //transform.translation += time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32);
-        transform.translation += (time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32));
-        transform.
+        //transform.translation += (time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32));
         //transform.translate_around(time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32),Quat::IDENTITY);
+        velocity.linvel += time.delta_seconds()  * direction;
 
     }
 }
+
+fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
+    for transform in positions.iter() {
+        println!("Ball ({},{},{}): ", transform.translation.x, transform.translation.y, transform.translation.z);
+    }
+}
+
+    
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierDebugRenderPlugin::default())
         .add_plugin(StarLustPlugin)
         .run();
 }
