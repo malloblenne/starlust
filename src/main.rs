e0230dc13ea2eecdf92f7334ac5cd4e1878d8a1f
Mauro <malloblenne@gmail.com>
2023-05-06 17:41:39 +0200

    Player: add player rs Move player implementation to separate rs

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 pub mod camera_handling;
 pub mod common_components;
+pub mod player;
 
 use crate::camera_handling::move_camera;
 use crate::camera_handling::spawn_camera;
 use crate::camera_handling::sync_player_camera;
-use crate::common_components::Player;
+use crate::player::move_player;
+use crate::player::spawn_player;
 
 #[derive(Component)]
 struct VelocityCustom {
@@ -67,40 +69,22 @@ fn populate_world(
     //     .spawn((Collider::cuboid(10.0, 0.1, 10.0),
     //     TransformBundle::from(Transform::from_xyz(0.0,  -1.0, 0.0))
     //     ));
-
     /* Create Player */
-    commands
-        .spawn(RigidBody::Dynamic)
-        .insert((
-            Collider::ball(0.5),
-            TransformBundle::from(Transform::from_xyz(1.0, 2.0, 0.0)),
-            Player,
-        ))
-        .insert(Restitution::coefficient(0.7))
-        .insert(Velocity {
-            linvel: Vec3::new(0.0, 0.0, 0.0),
-            angvel: Vec3::new(0.2, 0.0, 0.0),
-        })
-        .insert(GravityScale(0.0))
-        .insert(Damping {
-            linear_damping: 0.5,
-            angular_damping: 1.0,
-        })
-        .insert(Sleeping::disabled());
+    spawn_player(&mut commands);
 
-    commands
-        .spawn(RigidBody::Dynamic)
-        .insert((
-            Collider::ball(0.5),
-            TransformBundle::from(Transform::from_xyz(-5.0, 3.0, 0.0)),
-        ))
-        .insert(Restitution::coefficient(1.0))
-        // .insert(Velocity {
-        //     linvel: Vec3::new(0.0, 0.0, 0.0),
-        //     angvel: Vec3::new(0.2, 0.0, 0.0),
-        // })
-        .insert(GravityScale(0.5))
-        .insert(Sleeping::disabled());
+    // commands
+    //     .spawn(RigidBody::Dynamic)
+    //     .insert((
+    //         Collider::ball(0.5),
+    //         TransformBundle::from(Transform::from_xyz(-5.0, 3.0, 0.0)),
+    //     ))
+    //     .insert(Restitution::coefficient(1.0))
+    //     // .insert(Velocity {
+    //     //     linvel: Vec3::new(0.0, 0.0, 0.0),
+    //     //     angvel: Vec3::new(0.2, 0.0, 0.0),
+    //     // })
+    //     .insert(GravityScale(0.5))
+    //     .insert(Sleeping::disabled());
 }
 
 pub struct StarLustPlugin;
@@ -117,42 +101,6 @@ impl Plugin for StarLustPlugin {
         //.add_system(print_ball_altitude);
         //.add_system(hello_world)
         //.add_system(greet_people);
-    }
-}
-
-fn move_player(
-    input: Res<Input<KeyCode>>,
-    time: Res<Time>,
-    mut query: Query<(&mut Transform, &mut Velocity), With<Player>>,
-) {
-    for (mut transform, mut velocity) in &mut query {
-        let mut direction = Vec3::ZERO;
-        if input.pressed(KeyCode::Up) {
-            direction.y += 1.0;
-        }
-        if input.pressed(KeyCode::Down) {
-            direction.y -= 1.0;
-        }
-        if input.pressed(KeyCode::Left) {
-            direction.x -= 1.0;
-        }
-        if input.pressed(KeyCode::Right) {
-            direction.x += 1.0;
-        }
-
-        //transform.translation += time.delta_seconds() * 2.0 * direction;
-        // transform.rotate_axis(direction,
-        //                       time.delta_seconds() * 45f32.to_radians());
-
-        // velocity.x += time.delta_seconds() * ((input.pressed(KeyCode::Space) as i32) as f32 - 0.1f32);
-        // if velocity.x <= 0f32
-        // {
-        //     velocity.x = 0f32;
-        // }
-        //transform.translation += time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32);
-        //transform.translation += (time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32));
-        //transform.translate_around(time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32),Quat::IDENTITY);
-        velocity.linvel += time.delta_seconds() * direction;
     }
 }
 
---
+++ src/player.rs
@@ -0,0 +1,61 @@
+use crate::common_components::Player;
+use bevy::prelude::*;
+//use bevy_ecs::prelude::*;
+use bevy_rapier3d::prelude::*;
+
+pub fn spawn_player(commands: &mut Commands) {
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert((
+            Collider::ball(0.5),
+            TransformBundle::from(Transform::from_xyz(1.0, 2.0, 0.0)),
+            Player,
+        ))
+        .insert(Restitution::coefficient(0.7))
+        .insert(Velocity {
+            linvel: Vec3::new(0.0, 0.0, 0.0),
+            angvel: Vec3::new(0.2, 0.0, 0.0),
+        })
+        .insert(GravityScale(0.0))
+        .insert(Damping {
+            linear_damping: 0.5,
+            angular_damping: 1.0,
+        })
+        .insert(Sleeping::disabled());
+}
+
+pub fn move_player(
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &mut Velocity), With<Player>>,
+) {
+    for (mut transform, mut velocity) in &mut query {
+        let mut direction = Vec3::ZERO;
+        if input.pressed(KeyCode::Up) {
+            direction.y += 1.0;
+        }
+        if input.pressed(KeyCode::Down) {
+            direction.y -= 1.0;
+        }
+        if input.pressed(KeyCode::Left) {
+            direction.x -= 1.0;
+        }
+        if input.pressed(KeyCode::Right) {
+            direction.x += 1.0;
+        }
+
+        //transform.translation += time.delta_seconds() * 2.0 * direction;
+        // transform.rotate_axis(direction,
+        //                       time.delta_seconds() * 45f32.to_radians());
+
+        // velocity.x += time.delta_seconds() * ((input.pressed(KeyCode::Space) as i32) as f32 - 0.1f32);
+        // if velocity.x <= 0f32
+        // {
+        //     velocity.x = 0f32;
+        // }
+        //transform.translation += time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32);
+        //transform.translation += (time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32));
+        //transform.translate_around(time.delta_seconds() * Vec3::new(velocity.x, 0f32, 0f32),Quat::IDENTITY);
+        velocity.linvel += time.delta_seconds() * direction;
+    }
+}
