5e009b9f4535c3c63714abfe17fd41bbce3ac466
Mauro <malloblenne@gmail.com>
2023-05-06 17:59:10 +0200

    Remove commented code

--- src/main.rs
+++ src/main.rs
@@ -7,47 +7,9 @@ pub mod player;
 use crate::camera_handling::{move_camera, spawn_camera, sync_player_camera};
 use crate::player::{control_player, spawn_player};
 
-#[derive(Component)]
-struct VelocityCustom {
-    x: f32,
-}
-
 fn populate_world(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    //asset_server: Res<AssetServer>,
 ) {
-    //commands.spawn(BackgroundColor(Color::BLACK));
-
-    // ground plane
-    // commands.spawn(PbrBundle {
-    //     mesh: meshes.add(shape::Plane::from_size(10.0).into()),
-    //     material: materials.add(StandardMaterial {
-    //         base_color: Color::BLACK,
-    //         perceptual_roughness: 1.0,
-    //         ..default()
-    //     }),
-    //     ..default()
-    // });
-
-    // cube
-    // commands.spawn((
-    //     PbrBundle {
-    //         //mesh: meshes.add(Mesh::from(shape::RegularPolygon{sides: 3, radius: 1.0})),//shape::Cube { size: 1.0 })),
-    //         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-    //         material: materials.add(StandardMaterial {
-    //             base_color: Color::LIME_GREEN,
-    //             ..default()
-    //         }),
-    //         transform: Transform::from_xyz(0.0, 0.5, 0.0),
-    //         ..default()
-    //     },
-    //     Movable,
-    //     Player,
-    //     VelocityCustom{x: 0.0},
-    // ));
-
     // ambient light
     commands.insert_resource(AmbientLight {
         color: Color::WHITE,
@@ -62,42 +24,20 @@ fn populate_world(
         TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)),
     ));
 
-    // commands
-    //     .spawn((Collider::cuboid(10.0, 0.1, 10.0),
-    //     TransformBundle::from(Transform::from_xyz(0.0,  -1.0, 0.0))
-    //     ));
     /* Create Player */
     spawn_player(&mut commands);
 
-    // commands
-    //     .spawn(RigidBody::Dynamic)
-    //     .insert((
-    //         Collider::ball(0.5),
-    //         TransformBundle::from(Transform::from_xyz(-5.0, 3.0, 0.0)),
-    //     ))
-    //     .insert(Restitution::coefficient(1.0))
-    //     // .insert(Velocity {
-    //     //     linvel: Vec3::new(0.0, 0.0, 0.0),
-    //     //     angvel: Vec3::new(0.2, 0.0, 0.0),
-    //     // })
-    //     .insert(GravityScale(0.5))
-    //     .insert(Sleeping::disabled());
 }
 
 pub struct StarLustPlugin;
 
 impl Plugin for StarLustPlugin {
     fn build(&self, app: &mut App) {
-        //app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-        //app.insert_resource(ClearColor(Color::BLACK))
         app.add_startup_system(populate_world).add_systems((
             control_player,
             move_camera.after(control_player),
             sync_player_camera.after(control_player),
         ));
-        //.add_system(print_ball_altitude);
-        //.add_system(hello_world)
-        //.add_system(greet_people);
     }
 }
 
